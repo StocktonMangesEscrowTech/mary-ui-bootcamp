80f2be5a5c4c91a0e9d21b2321fe28ed9115c1db
stockton.manges <stockton.manges@escrowtech.com>
2025-02-21 11:08:23 -0700

    installed mary ui

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => '1c13cd4ebd9f90e9d90093ff360be6eb7fbe70cb',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => '1c13cd4ebd9f90e9d90093ff360be6eb7fbe70cb',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
